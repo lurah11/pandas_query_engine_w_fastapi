d1650caa95d41349a807ffeb8e1838110c3e42c7
Indrajid <indrajid.lurah@gmail.com>
2024-01-31 21:43:16 +0700

    update the frontend

--- pqe_fe/src/App.js
+++ pqe_fe/src/App.js
@@ -5,11 +5,34 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import ChatBar from './components/ChatBar';
 import Container from '@mui/material/Container'
+import Grid  from '@mui/material/Grid';
+import UploadBox from './components/UploadBox'
+import Typography  from '@mui/material/Typography';
+import ChatBox from './components/ChatBox';
+
 
 function App() {
   return (
     <Container >
-        <ChatBar></ChatBar>
+      <Typography fontFamily='Open Sans' sx={{'textAlign':'center','marginBottom':5, 'color':'cadetblue'}} component='h1' fontSize={40}>
+          QUERY YOUR DATAFRAME :D
+      </Typography>
+      <Grid container spacing={2}>
+          <Grid item xs={7}>
+            <ChatBar></ChatBar>
+          </Grid>
+          <Grid item xs={5}>
+            <UploadBox></UploadBox>
+          </Grid> 
+      </Grid>
+      <Grid container >
+        <Grid item xs={12}>
+            <Typography component="h5">
+                QUERY RESULT
+            </Typography>
+            <ChatBox />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
---
+++ pqe_fe/src/components/ChatBox.js
@@ -0,0 +1,10 @@
+import  Paper  from "@mui/material/Paper";
+
+
+export default function ChatBox() {
+    return (
+        <Paper sx={{'border':'2px solid blue', 'width':'100%', 'height':'300px'}} elevation={2}>
+        
+        </Paper>
+    )
+}
